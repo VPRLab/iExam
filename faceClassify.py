55c8d3d7e93fb058e1c233092c3ebade61bff680
Yang Xu <blitheyang99@gmail.com>
2021-05-28 11:18:28 +0800

    typeError correct

--- faceClassify.py
+++ faceClassify.py
@@ -1,12 +1,11 @@
 import cv2
 import os
-import shutil
 import pytesseract
 import difflib
 import platform
 
 
-def catchFaceAndClassify(dataset, name_lst, frame, num_frame):
+def catchFaceAndClassify(dataset, name_lst, frame, num_frame, viewInfo):
 
     classfier = cv2.CascadeClassifier("./haarcascades/haarcascade_frontalface_default.xml")
     if platform.system() == 'Windows':
@@ -15,15 +14,19 @@ def catchFaceAndClassify(dataset, name_lst, frame, num_frame):
         pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # change to greystyle
+    row = viewInfo.get('Row')
+    column = viewInfo.get('Column')
+    clip_width = int(viewInfo.get('Width') / row)
+    clip_height = int(viewInfo.get('Height') / column)
     faceRects = classfier.detectMultiScale(frame, 1.1, 5, minSize=(8, 8))   # objects are returned as a list of rectangles.
     if len(faceRects) > 0:
         for faceRect in faceRects:
             x, y, w, h = faceRect
 
-            for i in range(5):
-                for j in range(5):
-                    if 256*j <= x <= 256*(j + 1) and 144 * i <= y <= 144 * (i + 1):
-                        cropped = grey[144 * (i + 1) - 32:144 * (i + 1), 256 * j:256 * j + 120]  # decrease length
+            for i in range(column):
+                for j in range(row):
+                    if clip_width*j <= x <= clip_width*(j + 1) and clip_height * i <= y <= clip_height * (i + 1):
+                        cropped = grey[clip_height * (i + 1) - 32:clip_height * (i + 1), clip_width * j:clip_width * j + 120]  # decrease length
                         cropped = cv2.resize(cropped, None, fx=1.2, fy=1.2)
                         # if pixel greyscale>185, set this pixel=255, preprocess the character image to get good quality for OCR
                         ret, thresh1 = cv2.threshold(cropped, 185, 255, cv2.THRESH_TOZERO)
@@ -41,11 +44,11 @@ def catchFaceAndClassify(dataset, name_lst, frame, num_frame):
                         except Exception as e:
                             print("frame number:", num_frame, e)
                             pass
-                        else:
-                            # print('after text is:', text)
-                            clip_img = grey[y - 10:y + h + 10, x - 10:x + w + 10]
-                            if clip_img.size != 0 and text.isalpha():
-                                cv2.imwrite(dataset + '/' + text + '/{0}.jpg'.format(num_frame), clip_img)
+
+                        # print('after text is:', text)
+                        clip_img = grey[y - 10:y + h + 10, x - 10:x + w + 10]
+                        if clip_img.size != 0 and text.isalpha():
+                            cv2.imwrite(dataset + '/' + text + '/{0}.jpg'.format(num_frame), clip_img)
 
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
